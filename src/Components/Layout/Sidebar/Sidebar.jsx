3f882c5fd15e12e10218903334bf2a0511d49e19
vanhrr <vietanh22122003@gmail.com>
2024-09-02 10:51:45 +0700

    pagination sidebar

--- src/Components/Layout/Sidebar/Sidebar.jsx
+++ src/Components/Layout/Sidebar/Sidebar.jsx
@@ -1,13 +1,24 @@
 import style from "./Sidebar.module.scss";
 import classNames from "classnames/bind";
 import FoodItem from "../../FoodItem/FoodItem";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 const cx = classNames.bind(style);
 function Sidebar({ foods }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postPerPage = 10;
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage;
+  const numberOfPage = Math.ceil(foods.results / postPerPage);
+  const currentItems =
+    foods.foods && foods.foods.slice(firstPostIndex, lastPostIndex);
   return (
     <div className={cx("wrapper")}>
-      {foods.foods &&
-        foods.foods.map((item, index) => {
-          //console.log(item.publisher);
+      {currentItems &&
+        currentItems.map((item, index) => {
           return (
             <FoodItem
               key={index}
@@ -17,11 +28,45 @@ function Sidebar({ foods }) {
             />
           );
         })}
+      <div className={cx("footer")}>
+        {
+          <div className={cx("pagination")}>
+            {currentPage !== 1 && (
+              <button
+                onClick={() => {
+                  setCurrentPage((prev) => prev - 1);
+                }}
+                className={cx("decrease")}
+              >
+                <FontAwesomeIcon
+                  className={cx("iconLeft")}
+                  icon={faArrowLeft}
+                />{" "}
+                Page {currentPage - 1}
+              </button>
+            )}
+            {currentPage < numberOfPage && (
+              <button
+                onClick={() => {
+                  setCurrentPage((prev) => prev + 1);
+                }}
+                className={cx("increase")}
+              >
+                Page {currentPage + 1}{" "}
+                <FontAwesomeIcon
+                  className={cx("iconRight")}
+                  icon={faArrowRight}
+                />
+              </button>
+            )}
+          </div>
+        }
 
-      <p className={cx("copyRight")}>
-        © Copyright by Vanhrr. Use for learning or your portfolio. Don't use to
-        teach. Don't claim as your own.
-      </p>
+        <p className={cx("copyRight")}>
+          © Copyright by Vanhrr. Use for learning or your portfolio. Don't use
+          to teach. Don't claim as your own.
+        </p>
+      </div>
     </div>
   );
 }
